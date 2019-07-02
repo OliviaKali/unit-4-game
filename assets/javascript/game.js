fbbd3cc256da9b8713c9b292498bb2f61660ae29
OKali <o.kalinowski27@gmail.com>
2019-07-02 19:24:49 -0400

    Improved javascript so the game works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,109 @@
 $(document).ready(function () {
+
     var wins = 0;
     var losses = 0;
     var score = 0;
 
-    var randomNum = (Math.floor(Math.random() * 120) + 19);
-        //make sure randomNum formula is correct and only goes between 19-120
+    var randomNum = 50
+    //  (Math.floor(Math.random() * 102) + 19);
+    //make sure randomNum formula is correct and only goes between 19-120
 
     var crystal1 = (Math.floor(Math.random() * 12) + 1);
     var crystal2 = (Math.floor(Math.random() * 12) + 1);
     var crystal3 = (Math.floor(Math.random() * 12) + 1);
     var crystal4 = (Math.floor(Math.random() * 12) + 1);
 
-console.log(randomNum);
-console.log(crystal1);
-console.log(crystal2);
-console.log(crystal3);
-console.log(crystal4);
+    console.log("Random Number: " + randomNum);
+    console.log("crystal1 " + crystal1);
+    console.log("crystal2 " + crystal2);
+    console.log("crystal3: " + crystal3);
+    console.log("crystal4: " + crystal4);
 
-// $("#crystal1)").on("click", function() {
-//     console.log("Crystal1 has been clicked");
-// });
+    //for ( var i = 0; i <4; i++) {
 
-function reset () {
-    score = 0
+    $("#crystal-1").on("click", function () {
+        score = score + crystal1;
 
-    var crystal1 = (Math.floor(Math.random() * 12) + 1);
-    var crystal2 = (Math.floor(Math.random() * 12) + 1);
-    var crystal3 = (Math.floor(Math.random() * 12) + 1);
-    var crystal4 = (Math.floor(Math.random() * 12) + 1);
-    
+        checker(score)
+
+        console.log("now the score is: " + score);
+        // $("#score").html(score);
+    })
+
+    $("#crystal-2").on("click", function () {
+        score = score + crystal2;
+        checker(score)
+        console.log("now the score is: " + score);
+    })
+
+    $("#crystal-3").on("click", function () {
+        score = score + crystal3;
+        checker(score)
+        console.log("now the score is: " + score);
+    })
+
+    $("#crystal-4").on("click", function () {
+        score = score + crystal4;
+        checker(score)
+        console.log("now the score is: " + score);
+    })
+
+
+    function checker(number){
+        if(number === randomNum){
+            console.log("Winner");
+            wins++;
+        $("#win-text").text("Wins: " + wins)
+            reset();
+        }  
+        if(number > randomNum){
+            console.log("You lose!");
+            losses++;
+            $("#losses-text").text("Losses: " + losses)
+            reset();
+            //if number > randomNumber, then the losses go up.
+
+    }
 }
+
+    //}
+
+    // function ()
+    //  totalscore = score + crysal1 
+
+    function reset() {
+        score = 0
+
+        randomNum = (Math.floor(Math.random() * 102) + 19);
+        crystal1 = (Math.floor(Math.random() * 12) + 1);
+        crystal2 = (Math.floor(Math.random() * 12) + 1);
+        crystal3 = (Math.floor(Math.random() * 12) + 1);
+        crystal4 = (Math.floor(Math.random() * 12) + 1);
+
+        console.log(wins)
+        console.log(randomNum)
+        console.log("crystal1 " + crystal1);
+        console.log("crystal2 " + crystal2);
+        console.log("crystal3: " + crystal3);
+        console.log("crystal4: " + crystal4);
+    }
+
+    // if (score = randomNum) {
+    //     wins++
+    // }
+
+    // if (totalScore = randomNum) {
+    //     wins++
+    // }
+
+    // if (totalScore = randomNum)
+
+    // else if (score > randomNum) {
+    //     losses++
+    //     $("#losses-text").html(losses);
+    //     reset()
+    // }
+    // else user can continue to click the numbers to try to get the randomNum
 
 });
 
