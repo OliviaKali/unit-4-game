88387895fb512976b2cf084485c599c46e9f41f6
OKali <o.kalinowski27@gmail.com>
2019-07-02 21:20:22 -0400

    Commented out console.logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,13 +11,11 @@ $(document).ready(function () {
     var crystal3 = (Math.floor(Math.random() * 12) + 1);
     var crystal4 = (Math.floor(Math.random() * 12) + 1);
 
-    console.log("Random Number: " + randomNum);
-    console.log("crystal1 " + crystal1);
-    console.log("crystal2 " + crystal2);
-    console.log("crystal3: " + crystal3);
-    console.log("crystal4: " + crystal4);
-
-    //for ( var i = 0; i <4; i++) {
+    // console.log("Random Number: " + randomNum);
+    // console.log("crystal1 " + crystal1);
+    // console.log("crystal2 " + crystal2);
+    // console.log("crystal3: " + crystal3);
+    // console.log("crystal4: " + crystal4);
 
     $("#randomNumber").text("Random #: " + randomNum);
 
@@ -25,41 +23,40 @@ $(document).ready(function () {
         score = score + crystal1;
         checker(score)
         $("#totalScore").text(score);
-        console.log("now the score is: " + score);
-        // $("#score").html(score);
+        // console.log("now the score is: " + score);
     })
 
     $("#crystal-2").on("click", function () {
         score = score + crystal2;
         checker(score)
         $("#totalScore").text(score);
-        console.log("now the score is: " + score);
+        // console.log("now the score is: " + score);
     })
 
     $("#crystal-3").on("click", function () {
         score = score + crystal3;
         checker(score)
         $("#totalScore").text(score);
-        console.log("now the score is: " + score);
+        // console.log("now the score is: " + score);
     })
 
     $("#crystal-4").on("click", function () {
         score = score + crystal4;
         checker(score)
         $("#totalScore").text(score);
-        console.log("now the score is: " + score);
+        // console.log("now the score is: " + score);
     })
 
 
     function checker(number) {
         if (number > randomNum) {
-            console.log("You lose!");
+            // console.log("You lose!");
             losses++;
             $("#losses-text").text("Losses: " + losses)
             reset();
         }
         if (number === randomNum) {
-            console.log("Winner");
+            // console.log("Winner");
             wins++;
             $("#win-text").text("Wins: " + wins)
             reset();
@@ -77,24 +74,11 @@ $(document).ready(function () {
 
         $("#randomNumber").text("Random #: " + randomNum);
 
-        console.log(wins)
-        console.log(randomNum)
-        console.log("crystal1 " + crystal1);
-        console.log("crystal2 " + crystal2);
-        console.log("crystal3: " + crystal3);
-        console.log("crystal4: " + crystal4);
+        // console.log(wins)
+        // console.log(randomNum)
+        // console.log("crystal1 " + crystal1);
+        // console.log("crystal2 " + crystal2);
+        // console.log("crystal3: " + crystal3);
+        // console.log("crystal4: " + crystal4);
     }
 });
-
-    //assign a random number generator (1-12) for each crystal
-    //assign button/onclick for each crystal 
-    //Make the score appear whenever a crystal is clicked
-    //(Score needs to be added everytime a crystal is clicked)
-    //use for loop for crystals
-    //Make the wins and losses go up based on if they win or lose
-    //write if/else statement concerning score = random #
-    //if score = randomNum wins++
-    //else score > randomNum losses++
-    //reset items on the page randomNum, 
-        //crystals, total score so game can be played again
-    //make score and wins appear on html page    
